e9bfa6657130928cebe0ded13df5a1e57ed9f2eb
impostor692 <77915398+impostor692@users.noreply.github.com>
2021-03-05 22:49:57 +0300

    Create fastafile.java

---
+++ fastafile.java
@@ -0,0 +1,26 @@
+import.fileconvert
+import.os
+import.file
+import.all
+    @Ovveride
+      javaClassToken(FileConvert)
+   convertFile(convert.tokens)
+      @Ovveride
+        fasta!( seq.file)
+        pdb!( protein.file)
+    @Ovveride
+       main_frame="Client"
+          protein( seq.file
+    amino_acid = ( sequence.file
+          a-w-q-...( amino.acids)
+     amino_acid_visualizer(cilent.id = convert)
+     acid_frame_visual( amino_acid
+        protein( carbon_alfa
+          isAminoAcid
+                 visual.alfa(carbon)
+           frame="amino_acid"
+            
+          
+    
+             
+      
